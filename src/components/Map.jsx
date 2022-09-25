2c75a3c52d1b2c8f86d49e5bc9582388ee6b5004
Fengji Zhang <thewyze9@outlook.com>
2022-09-25 13:25:31 -0400

    added markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,42 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import '../cssFiles/map.css';
+// import issLogo from '../imgFiles/ISS.png';
+
 const axios = require('axios');
 
 function Map() {
 
-    const [coordindates, setC] = useState([[],[]]);
-
     const [currentCoords, setCurrentC] = useState([0.0, 0.0]);
     
-    if (currentCoords[0] === 0.0) {
+    useEffect( () => {
 
-        axios.get('http://api.open-notify.org/iss-now.json')
-        .then( (res) => {
-            // handle success
-            console.log(res);
-            // setCurrentC([res.data.iss_position.latitude,res.data.iss_position.longitude]);
-        })
-        .catch((err) => {
-            // handle error
-            console.log(err);
-        })
+        if (currentCoords[0] === 0.0) {
 
-    }
+            axios.get('http://api.open-notify.org/iss-now.json')
+            .then( (res) => {
+                // handle success
+                console.log(res);
+                setCurrentC([res.data.iss_position.latitude,res.data.iss_position.longitude]);
+            })
+            .catch((err) => {
+                // handle error
+                console.log(err);
+            })
     
+        }
+
+    })
+
+    
+
+
+
     //TODO --edit
     return (
         <div className="headerTop">
-            <p>{coordindates}</p>
-
-            <MapContainer center={[currentCoords[0], currentCoords[1]]} zoom={5} scrollWheelZoom={true} className="mapContainer">
+            {console.log('test!', currentCoords.toString())}
+            <MapContainer center={currentCoords} zoom={2} scrollWheelZoom={true} className="mapContainer">
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[currentCoords[0], currentCoords[1]]}>
+                <Marker position={currentCoords}>
+                    
                     <Popup>
-                    Current ISS Location<br /> 
+                    International Space Station<br /> 
+                    Longitude: {currentCoords[0]} <br />
+                    Latitude: {currentCoords[1]}
                     </Popup>
                 </Marker>
                 </MapContainer>
